df93cd477f8f558c3d89d8ef49513b94ccddd295
Wesley Thompson <wesleythompson1995@gmail.com>
2018-01-22 19:49:20 -0600

    Mostly complete pistol animation+animator+controller script

---
+++ Assets/-Pistolero-/Scripts/PistolController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistolController : MonoBehaviour {
+
+    public int bulletCapacity;
+    public int bulletCount;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        if(!animator)
+        {
+            Debug.LogError("No Animator detected");
+        }
+
+        SetBulletCount(bulletCount);
+    }
+
+    public void Fire()
+    {
+        bulletCount--;
+        SetBulletCount(bulletCount);
+        animator.SetTrigger("Fire");
+    }
+
+    public void Reload()
+    {
+        bulletCount = bulletCapacity;
+        animator.SetTrigger("Reload");
+    }
+
+    private void SetBulletCount(int bulletCount)
+    {
+        animator.SetInteger("Bullets", bulletCount);
+    }
+}
