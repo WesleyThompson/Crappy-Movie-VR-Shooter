ca49682c3b79eb82d373d60151b60025158a36e1
Wesley Thompson <wesleythompson1995@gmail.com>
2018-01-22 23:42:39 -0600

    integrating pistol with vrtk WIP

--- Assets/-Pistolero-/Scripts/PistolController.cs
+++ Assets/-Pistolero-/Scripts/PistolController.cs
@@ -1,36 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
+[RequireComponent(typeof(Animator))]
 public class PistolController : MonoBehaviour {
 
+    public VRTK_ControllerEvents controllerEvents;
     public int bulletCapacity;
     public int bulletCount;
 
+    [SerializeField] private AudioSource fireSound;
+    [SerializeField] private AudioSource reloadSound;
+    [SerializeField] private AudioSource dropMagSound;
+    [SerializeField] private AudioSource emptyMagFireSound;
+
+    private bool isEmpty = false;
     private Animator animator;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
-        if(!animator)
+
+        SetBulletCount(bulletCount);
+        if(bulletCount == 0)
         {
-            Debug.LogError("No Animator detected");
+            isEmpty = true;
+            animator.Play("PistolMovingPartsEmpty");
         }
 
-        SetBulletCount(bulletCount);
+        //Setup events
+        controllerEvents.SubscribeToButtonAliasEvent(VRTK_ControllerEvents.ButtonAlias.TriggerClick, true, Fire);
     }
 
-    public void Fire()
+    public void Fire(object sender, ControllerInteractionEventArgs e)
     {
-        bulletCount--;
-        SetBulletCount(bulletCount);
-        animator.SetTrigger("Fire");
+        if(!isEmpty)
+        {
+            fireSound.Play();
+            bulletCount--;
+            SetBulletCount(bulletCount);
+            if (bulletCount == 0)
+            {
+                isEmpty = true;
+            }
+            animator.SetTrigger("Fire");
+        }
+        else
+        {
+            emptyMagFireSound.Play();
+        }
     }
 
-    public void Reload()
+    public void Reload(object sender, ControllerInteractionEventArgs e)
     {
+        reloadSound.Play();
         bulletCount = bulletCapacity;
+        isEmpty = false;
         animator.SetTrigger("Reload");
+    }
+
+    public void DropMag(object sender, ControllerInteractionEventArgs e)
+    {
+        bulletCount = 0;
+        SetBulletCount(bulletCount);
+        animator.Play("PistolMovingPartsEmpty");
+        dropMagSound.Play();
     }
 
     private void SetBulletCount(int bulletCount)
