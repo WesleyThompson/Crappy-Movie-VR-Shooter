bd4707c2266f8608c52dc558d790fa88fd33e39f
oliviaapperson <ocapperson@hotmail.com>
2018-01-27 01:01:14 -0600

    Added enemy dying script

---
+++ Assets/-Pistolero-/Scripts/Enemy Scripts/EnemyDying.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyDying : MonoBehaviour {
+
+    private Animator animator;
+    private NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            EnemyDies();
+        }
+	}
+
+    public void EnemyDies()
+    {
+        animator.SetTrigger("Die");
+        navMeshAgent.isStopped = true;
+    }
+}
--- Assets/-Pistolero-/Scripts/Enemy Scripts/SetOffset.cs
+++ Assets/-Pistolero-/Scripts/Enemy Scripts/SetOffset.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SetOffset : MonoBehaviour {
 
+
+
 	// Use this for initialization
 	void Start () {
 		
